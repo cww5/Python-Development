5d70f82ffa2bc438904359767e721e7de5b36be6
Connor Watson <connorwatson0811@gmail.com>
2021-03-16 11:41:08 -0400

    20210316 - copy emails and phone numbers back to clipboard

--- phoneNumberExtractor.py
+++ phoneNumberExtractor.py
@@ -9,7 +9,7 @@ TODO:
 >>DONE 2. Create a regex object for email addresses
 >>DONE 3. Get text from clipboard
 >>DONE 4. Extract emails / phone numbers from the text
-5. Copy extracted emails / phone numbers to key board
+>>DONE 5. Copy extracted emails / phone numbers to key board
 
 '''
 
@@ -26,23 +26,25 @@ def create_phone_email_regex():
     '''
     phone_number_regex = re.compile(
         r'''
-        (\d\d\d-) |      # XXX-
-        (\(\d\d\d\)-) |  # (XXX)-
-        (\(\d\d\d\) )    # (XXX) 
-        \d{3}           # XXX
-        -
-        \d{4}           # XXXX
-        (\sx\d{2,5})?
+        (
+        ((\d{3})|(\(\d{3}\)))?  # optional area code
+        (\s|-)                  # first separator
+        \d{3}                   # first 3 digits
+        -                       # second separator
+        \d{4}                   # last 4 digits
+        (((ext(\.)?\s)|x)       # extension word-part (optional)
+        (\d{2,5}))?             # extension number part (optional)
+        )
         ''', re.VERBOSE
     )
-    phone_number_regex = re.compile(r'\(\d{3}\) \d{3}-\d{4}')
+    # phone_number_regex = re.compile(r'\(\d{3}\) \d{3}-\d{4}')
     email_address_regex = re.compile(r'([a-z|A-Z]|[0-9]|_)*\@[a-zA-Z]+\.[a-z]+')
-    email_address_regex = re.compile(r'[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]+')
+    email_address_regex = re.compile(r'[a-zA-Z0-9_.]+@[a-zA-Z0-9]+\.[a-z]+')
     return phone_number_regex, email_address_regex
 
 
 def get_phone_numbers_from_text(text, phone_regex):
-    phone_list = phone_regex.findall(text)
+    phone_list = [tup[0] for tup in phone_regex.findall(text)]
     return phone_list
 
 
@@ -51,7 +53,7 @@ def get_emails_from_text(text, email_regex):
     return email_list
 
 
-def put_numbers_emails_to_clipboard(numbers, emails):
+def put_numbers_emails_to_clipboard(numbers, emails, to_copy=False):
     if len(numbers) > 1:
         numbers_string = ','.join(numbers)
     else:
@@ -60,18 +62,18 @@ def put_numbers_emails_to_clipboard(numbers, emails):
         emails_string = ','.join(emails)
     else:
         emails_string = str(emails[0])
-    pyperclip.copy(numbers_string + emails_string)
+    print(numbers_string, emails_string)
+    if to_copy:
+        pyperclip.copy(numbers_string + emails_string)
 
 
 def main():
     copied_text = get_text_from_clipboard()
-    print(copied_text)
     phone_regex, email_regex = create_phone_email_regex()
     all_numbers = get_phone_numbers_from_text(copied_text, phone_regex)
-    print(all_numbers)
     all_emails = get_emails_from_text(copied_text, email_regex)
-    print(all_emails)
-    put_numbers_emails_to_clipboard(all_numbers, all_emails)
+    put_numbers_emails_to_clipboard(all_numbers, all_emails, to_copy=False)
+
 
 if __name__ == '__main__':
     main()
