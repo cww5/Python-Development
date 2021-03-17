cf9c97a99b139a8604761c7fad684836a6b8ba48
Connor Watson <connorwatson0811@gmail.com>
2021-03-16 22:01:16 -0400

    20210316 - push new files

--- filesnotes.py
+++ filesnotes.py
@@ -1,6 +1,7 @@
 #! python3
 # working with files / paths
 import os
+import shelve
 
 
 def files_notes():
@@ -14,13 +15,46 @@ def files_notes():
     print(os.path.join('C', 'dir', 'dir', 'filename.ext'))
     print(os.sep) # separator in file paths
     print(os.getcwd())
-    os.chdir('C:\\')
-    print(os.getcwd())
+    # os.chdir('C:\\')
+    # print(os.getcwd())
+    print(os.path.abspath('point.py'))
+    print(os.path.abspath('..\\..')) # absolute path 2 directories up
+    print(os.path.isabs('..\\..'))
+    print(os.path.relpath('c:\\f1\\f2\\spam.png', 'c:\\f1'))
+    print(os.path.dirname('c:\\f1\\f2\\spam.png'))
+    print(os.path.basename('c:\\f1\\f2\\spam.png'))
+    print(os.path.exists('c:\\f1\\f2\\spam.png'))
+    print(os.path.isfile('c:\\f1\\f2\\spam.png'))
+    print(os.path.isdir('c:\\f1\\f2\\spam.png'))
+    # print(os.path.getsize('c:\\f1\\f2\\spam.png'))
+    print(os.listdir(os.getcwd()))
 
+
+def get_file_sizes(my_folder=os.getcwd()):
+    print('Searching in: ' + my_folder)
+    for sub_path in os.listdir(my_folder):
+        if os.path.isfile(sub_path):
+            print(sub_path, os.path.getsize(sub_path))
+
+
+def shelve_file_creation():
+    shelve_file = shelve.open('Data\\sampleshelve')
+    shelve_file['dogs'] = ['Yorkshire', 'Poodle', 'Greyhound']
+    shelve_file.close()
+    shelve_test = shelve.open('Data\\sampleshelve')
+    print(shelve_test['dogs'])
+    shelve_test.close()
+
+
+def create_simple_file():
 
 def main():
     print()
     files_notes()
+    my_folder = os.path.abspath('..\\')
+    get_file_sizes(my_folder)
+    get_file_sizes()
+    shelve_file_creation()
 
 
 if __name__ == '__main__':
